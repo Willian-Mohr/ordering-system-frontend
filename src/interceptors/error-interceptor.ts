92b8e8fe98e71f9ad3c6c64ba9f18c6126b55d24
Willian Junior Brizola Mohr <53913365+Willian-Mohr@users.noreply.github.com>
2021-09-03 19:00:03 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
 
     }
 
@@ -26,17 +27,51 @@ export class ErrorInterceptor implements HttpInterceptor {
                 console.log(errorObj);
 
                 switch (errorObj.status) {
-                    case 403:
-                        this.handle403();
+                    case 401:
+                        this.handle401();
                         break;
+
+                    case 403:
+                        this.handler403();
+                        break;
+
+                    default:
+                        this.handlerDefaultError(errorObj);
                 }
 
                 return Observable.throw(errorObj);
             }) as any;
     }
 
-    handle403() {
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: Falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handler403() {
         this.storage.setLocalUser(null);
+    }
+    handlerDefaultError(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ':' + errorObj.error,
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
