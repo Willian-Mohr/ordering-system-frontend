9f3b88db1ee44e1526aab92b04c7c21f8edf4c96
Willian Junior Brizola Mohr <53913365+Willian-Mohr@users.noreply.github.com>
2021-09-05 17:58:59 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -25,6 +25,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  private loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -65,4 +69,16 @@ export class ProfilePage {
     });
   }
 
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(reponse => {
+        this.picture = null;
+        this.loadData();
+      },
+        error => { });
+  }
+
+  cancel() {
+    this.picture = null;
+  }
 }
