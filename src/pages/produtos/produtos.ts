0b3df8d3b9935f8c7ab3ff931f75724df3fbe1d3
Willian Junior Brizola Mohr <53913365+Willian-Mohr@users.noreply.github.com>
2021-09-05 14:25:06 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -12,7 +12,8 @@ import { ProdutoService } from '../../services/domain/produto.service';
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] = [];
+  page: number = 0;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
@@ -27,19 +28,23 @@ export class ProdutosPage {
   private loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe(response => {
-        this.items = response['content'];
+        let start = this.items.length;
+        this.items = this.items.concat(response['content']);
+        let end = this.items.length - 1;
         loader.dismiss();
-        this.loadImageUrls();
+        console.log(this.page);
+        console.log(this.items);
+        this.loadImageUrls(start, end);
       },
         error => {
           loader.dismiss();
         });
   }
 
-  loadImageUrls() {
-    for (let i = 0; i < this.items.length; i++) {
+  loadImageUrls(start: number, end: number) {
+    for (let i = start; i <= end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -62,6 +67,16 @@ export class ProdutosPage {
   }
 
   doRefresh(event) {
+    this.page = 0;
+    this.items = [];
+    this.loadData();
+    setTimeout(() => {
+      event.complete();
+    }, 1000);
+  }
+
+  doInfinite(event) {
+    this.page++
     this.loadData();
     setTimeout(() => {
       event.complete();
